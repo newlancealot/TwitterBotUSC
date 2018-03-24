21dbdaefe9a148890d9f0391da5eaec761d50cc2
newlancealot <33213879+newlancealot@users.noreply.github.com>
2018-03-24 11:24:06 -0700

    fixing errors

--- heroku_bot.py
+++ heroku_bot.py
@@ -18,10 +18,10 @@ analyzer = SentimentIntensityAnalyzer()
 #from config import access_tokensecretTwitter
 
 #This is how i hide my API keys in heroku
-api_keyTwitter= os.environ['']
-pi_secretTwitter= os.environ['']
-access_tokenTwitter= os.environ['']
-access_tokensecretTwitter= os.environ['']
+api_keyTwitter= os.environ['api_keyTwitter']
+api_secretTwitter= os.environ['api_secretTwitter']
+access_tokenTwitter= os.environ['access_tokenTwitter']
+access_tokensecretTwitter= os.environ['access_tokensecretTwitter']
 
 # Setup Tweepy API Authentication
 
@@ -33,13 +33,13 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 # Quotes to Tweet
 happy_quotes = [
-    "For every minute you are angry you lose sixty seconds of happiness. - Ralph Waldo Emerson",
-    "Folks are usually about as happy as they make their minds up to be. - Abraham Lincoln",
-    "Happiness is when what you think, what you say, and what you do are in harmony. - Mahatma Gandhi",
-    "Count your age by friends, not years. Count your life by smiles, not tears. - John Lennon",
-    "Happiness is a warm puppy. - Charles M. Schulz",
-    "The happiness of your life depends upon the quality of your thoughts. - Marcus Aurelius",
-    "Now and then it's good to pause in our pursuit of happiness and just be happy. - Guillaume Apollinaire"]
+    "yeah buddy. - Ralph Waldo Emerson",
+    "drive it like you stole it. - Abraham Lincoln",
+    "I got a bad feeling about this. - Mahatma Gandhi"]
+    #"One does not simple walk into Mordor - John Lennon",
+    #"Happiness is a warm puppy. - Charles M. Schulz",
+    #"The happiness of your life depends upon the quality of your thoughts. - Marcus Aurelius",
+#    "Now and then it's good to pause in our pursuit of happiness and just be happy. - Guillaume Apollinaire"]
 
 
 # Create function for tweeting
